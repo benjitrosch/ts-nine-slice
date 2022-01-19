9543498274ede253e4415289df971226f9f41a27
benjitrosch <benjitrosch@gmail.com>
2022-01-19 05:35:44 -0500

    moved index.html to root folder

--- src/main.ts
+++ src/main.ts
@@ -12,64 +12,15 @@ window.onload = function() {
     canvas.width = width
     canvas.height = height
 
-    let mouseX = 0
-    let mouseY = 0
-    let mousedown = false
-    let selection: AABB | null = null
-
     const box = new AABB(width / 2, height / 2, 50, 50)
 
-    const nineslice = new NineSlice("16x16_window_alt.png", 55, 135, 20, 135, () => draw(context))
-    const pattern = new Pattern(context, "background_pattern.png")
+    const nineslice = new NineSlice("./src/16x16_window.png", 55, 135, 20, 135, () => draw(context))
+    const pattern = new Pattern(context, "./src/background_pattern.png")
     const panel = new Panel(canvas, nineslice, width / 4, height / 8, 0, width / 2, height / 2)
 
-    document.body.addEventListener("mousedown", (e) => {
-        const { x, y } = getMousePos(canvas, e)
-
-        mouseX = x
-        mouseY = y
-        mousedown   = true
-
-        if (box.check(mouseX, mouseY)) {
-            selection = box
-        }
-    })
-
     document.body.addEventListener("mousemove", (e) => {
-        const { x, y } = getMousePos(canvas, e)
-
         draw(context)
-
-        if (mousedown)
-        {
-            const deltaX = mouseX - x
-            const deltaY = mouseY - y
-
-            mouseX = x
-            mouseY = y
-
-            if (selection != null) {
-                selection.x -= deltaX
-                selection.y -= deltaY
-
-                draw(context)
-            }
-        }
     })
-
-    document.body.addEventListener("mouseup", (e) => {
-        mousedown   = false
-        selection   = null
-    })
-
-    function getMousePos(canvas: HTMLCanvasElement, e: MouseEvent) {
-        const rect = canvas.getBoundingClientRect()
-
-        return {
-          x: e.clientX - rect.left,
-          y: e.clientY - rect.top
-        };
-    }
 
     function draw(context: CanvasRenderingContext2D) {
         context.clearRect(0, 0, width, height)
